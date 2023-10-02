1a18865eb4a0d3dcda0cd6114a6b6a2ebad0741f
deannate00 <deannate00@gmail.com>
2023-10-02 16:51:09 -0400

    MultiSheet Views and GeoReader

---
+++ SwiftConcepts/GeoReader.swift
@@ -0,0 +1,18 @@
+//
+//  GeoReader.swift
+//  SwiftConcepts
+//
+//  Created by Nathan Leach on 10/2/23.
+//
+
+import SwiftUI
+
+struct GeoReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeoReader()
+}
---
+++ SwiftConcepts/MultiSheets.swift
@@ -0,0 +1,18 @@
+//
+//  MultiSheets.swift
+//  SwiftConcepts
+//
+//  Created by Nathan Leach on 10/2/23.
+//
+
+import SwiftUI
+
+struct MultiSheets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiSheets()
+}
