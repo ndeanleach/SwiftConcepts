3b6cb5a86e5226acba0e41131e145ac1398b529a
deannate00 <deannate00@gmail.com>
2023-10-03 13:46:42 -0400

    Different haptic vibrations

---
+++ SwiftConcepts/HapticsView.swift
@@ -0,0 +1,18 @@
+//
+//  HapticsView.swift
+//  SwiftConcepts
+//
+//  Created by Nathan Leach on 10/3/23.
+//
+
+import SwiftUI
+
+struct HapticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticsView()
+}
